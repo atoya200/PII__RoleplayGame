4b61d0c8e3ae5ddb563faf6e76ba6a191867fcc0
atoya200 <agustintoya2004@gmail.com>
2022-09-28 05:52:00 -0300

    .

--- src/Library/Arma.cs
+++ src/Library/Arma.cs
@@ -7,6 +7,11 @@ namespace Library
         public string Descripcion { get; private set; }
         public int Ataque { get; private set; }
         public int Defensa { get; private set; }
+        /* 
+         El constructor esta diseñado para que verifique si el valor ingresado es correcto.
+         En caso de serlo asigna el valor recibido a la propiedad correspondiente.
+         En el caso contrario le asigna null a la propiedad como forma de no dejar pasar datos erroneos.
+        */ 
         public Arma(string name, string descripcion, int ataque, int defensa)
         {
             if (TextoValido(name))
@@ -42,15 +47,27 @@ namespace Library
                 this.Defensa = 0;
             }
         }
-        public bool TextoValido(string name)
+        /*
+         Este método revisa si el string que le llega es nulo, vacío
+         o tiene un formato inadecuado. En cualquiera de los tres casos
+         devuelve false, ya que el texto no es valido. Si el formato es 
+         correcto y no es ni un string vacío ni un null, devuelve true.  
+        */
+        public bool TextoValido(string texto)
         {
-            if (name == null || name.Length == 0 || NoTieneLetrasNumeros(name))
+            if (texto == null || texto.Length == 0 || NoTieneLetrasNumeros(texto))
             {
                 return false;
             }
             return true;
         }
 
+        /*
+         Este método lo que hace es verificar que el texto esta correcto o no.
+         En caso de no tener letras, números o algunos simbolos devolverá true,
+         ya que no es un formato correcto. En caso de que tenga el formato adecuado
+         devolvera false. 
+        */
         public bool NoTieneLetrasNumeros(string texto)
         {
             List<char> letras = new List<char>() { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z' };
@@ -60,7 +77,6 @@ namespace Library
             List<char> conTilde = new List<char>{'á', 'é', 'í', 'ó', 'ú', 'Á', 'É', 'Ú','Í','Ó'};
             // Le quitamos los posibles espacios que pueda llegar a tener adelante y atras.
             texto = texto.Trim();
-
             // Una var para ir revisando que todos los caracteres esten correctos.
             bool formatoIncorrecto = true;
             foreach (char c in texto)
@@ -76,6 +92,11 @@ namespace Library
             return formatoIncorrecto;
         }
 
+        /* 
+         Este método se encarga de validar que un valor no sea inferior a cero.
+         Si ese es el caso devuelve false, y en el caso contrario devuleve true, 
+         ya que el valor será 0 o mayor que cero.
+        */
         public bool ValorMayorIgualCero(int valor)
         {
             if (valor < 0)
